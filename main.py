a68f908b5795bb7e16ae181ece3a8f0f2b9cd021
DanDC25 <111406215+DanDC25@users.noreply.github.com>
2022-10-21 10:14:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ knob_pressed = False
 starting_time = control.millis()
 currentState = IDLE
 currentTime = 0
-        
+
 def onPause_until():
     return False
     pause_until(button_B_pressed)
 
-def Volume():
+def Volume(int: volume):
     pass
 
-def Spin():
+def Spin(int: spin):
     pass
 
-def WashingLiquid():
+def Softener(int: softener):
     pass
 
+def Detergent(int: detergent):
+    pass
 
 
 def playSound():
@@ -74,12 +76,31 @@ def get_knob_data():
     return Math.map(analogValue, 0, 1023, 0, 270)
 
 def LightWash():
+    Volume(1000)
+    Spin(400)
+    Softener(5)
+    Detergent(5)
+    serial.write_line("Water 1000militer")
+    serial.write_line("Detergent 5militer")
+    serial.write_line("Softener 5militer")
+    serial.write_line("400RPM")
 
 
-def 
+
+
 
 def HeavyWash():
+    Volume(2000)
+    Spin(400)
+    Softener(10)
+    Detergent(10)
+    serial.write_line("Water 2liters")
+    serial.write_line("Detergent 10militer")
+    serial.write_line("Softener 10militer")
+    serial.write_line("400RPM")
+
     
+
 
 def updateSystem():
     starting_time
