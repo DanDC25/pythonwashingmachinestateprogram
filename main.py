39667671cdf8e65e69998047eed038801525a57c
DanDC25 <111406215+DanDC25@users.noreply.github.com>
2022-10-30 18:07:32 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ starting_time = control.millis()
 currentState = IDLE
 currentTime = 0
 
-def onPause_until():
-    return False
-    pause_until(button_B_pressed())
+statePrinted = False
+
+soundPrinted = False
+
+TurnDegree = 270.0
 
 def Volume(int: volume):
     pass
@@ -33,8 +35,10 @@ def Detergent(int: detergent):
 
 
 def playSound():
-    for i in range(4):
-        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    if soundPrinted == False:
+        for i in range(4):
+            music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        soundPrinted = True
 
 def on_button_pressed_a():
     global button_A_pressed
@@ -46,8 +50,9 @@ def on_button_pressed_b():
     button_B_pressed = True
     pass
 
-def TurnDegree():
-    TurnDegree = get_knob_data()
+def MeasureTurnDegree():
+    someDegree = get_knob_data()
+    return someDegree
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
 input.on_button_pressed(Button.B, on_button_pressed_b)
@@ -57,24 +62,30 @@ def updateSystem():
     global currentTime
     global button_A_pressed
     global button_B_pressed
+
+    if (input.button_is_pressed(Button.B)):
+        button_B_pressed = True
     
+
+
+def evaluateState(state: int):
     if currentState == IDLE:
         start_time_ms = control.millis()
         if(button_B_pressed):
-            #select desired washingtype
-            pass
+            return WASHING
         if (button_A_pressed):
             currentTime = 0
+            return DONE
     if(currentState == WASHING):
         current_time_ms = control.millis()
         if(current_time_ms - start_time_ms) > 1000:
             currentTime -= 1
             start_time_ms = current_time_ms
+            return DONE
+        if (button_A_pressed):
+            return DONE
+    return state
 
-def evaluateState(state: int):
-    if state == IDLE:
-        if button_B_pressed:
-            return WASHING
 
 
 def reactToState(int: currentState):
@@ -99,11 +110,11 @@ def reactToState(int: currentState):
 
     if(currentState == IDLE): 
         basic.show_leds("""
+                . . # . .
+                # . # . #
+                # . # . #
+                # . . . #
                 . # # # .
-                . # . # .
-                . # # # .
-                . # . . .
-                . # . . .
         """)
         serial.write_line("Idle")
 
@@ -118,6 +129,13 @@ def reactToState(int: currentState):
         serial.write_line("Paused")
 
     if(currentState == DONE):
+        basic.show_leds("""
+            # # # . .
+                # . . # .
+                # . . # .
+                # . . # .
+                # # # . .
+        """)
         playSound()
 
 
@@ -145,10 +163,13 @@ def HeavyWash():
     Spin(400)
     Softener(10)
     Detergent(10)
-    serial.write_line("Water 2liters")
-    serial.write_line("Detergent 10militer")
-    serial.write_line("Softener 10militer")
-    serial.write_line("400RPM")
+    if statePrinted == False:
+        serial.write_line("Water 2liters")
+        serial.write_line("Detergent 10militer")
+        serial.write_line("Softener 10militer")
+        serial.write_line("400RPM")
+        statePrinted = True
+
 
 
 
--- main.ts
+++ main.ts
@@ -6,21 +6,188 @@ let DONE = 4
 let OFF = 5
 let button_A_pressed = false
 let button_B_pressed = false
+let knob_pressed = false
 let starting_time = control.millis()
+let currentState = IDLE
 let currentTime = 0
+let statePrinted = false
+let soundPrinted = false
+let TurnDegree = 270.0
+function Volume(int: number) {
+    
+}
+
+function Spin(int: number) {
+    
+}
+
+function Softener(int: number) {
+    
+}
+
+function Detergent(int: number) {
+    
+}
+
 function playSound() {
-    for (let i = 0; i < 4; i++) {
-        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    let soundPrinted: boolean;
+    if (soundPrinted == false) {
+        for (let i = 0; i < 4; i++) {
+            music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        }
+        soundPrinted = true
     }
+    
+}
+
+function MeasureTurnDegree(): number {
+    let someDegree = get_knob_data()
+    return someDegree
 }
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    let button_A_was_pressed = true
+    button_A_pressed = true
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    let button_B_was_pressed = true
+    button_B_pressed = true
     
+})
+function updateSystem() {
+    
+    
+    
+    
+    if (input.buttonIsPressed(Button.B)) {
+        button_B_pressed = true
+    }
+    
+}
+
+function evaluateState(state: number): number {
+    let start_time_ms: number;
+    let currentTime: number;
+    let current_time_ms: number;
+    if (currentState == IDLE) {
+        start_time_ms = control.millis()
+        if (button_B_pressed) {
+            return WASHING
+        }
+        
+        if (button_A_pressed) {
+            currentTime = 0
+            return DONE
+        }
+        
+    }
+    
+    if (currentState == WASHING) {
+        current_time_ms = control.millis()
+        if (current_time_ms - start_time_ms > 1000) {
+            currentTime -= 1
+            start_time_ms = current_time_ms
+            return DONE
+        }
+        
+        if (button_A_pressed) {
+            return DONE
+        }
+        
+    }
+    
+    return state
+}
+
+function reactToState(int: number) {
+    if (currentState == WASHING) {
+        if (TurnDegree > 135) {
+            HeavyWash()
+        } else {
+            LightWash()
+        }
+        
+        basic.showLeds(`
+                # . # . #
+                # . # . #
+                # . # . #
+                # . # . #
+                . # . # .
+        `)
+        serial.writeLine("Washing")
+    }
+    
+    if (currentState == IDLE) {
+        basic.showLeds(`
+                . . # . .
+                # . # . #
+                # . # . #
+                # . . . #
+                . # # # .
+        `)
+        serial.writeLine("Idle")
+    }
+    
+    if (currentState == PAUSED) {
+        basic.showLeds(`
+                . # # # .
+                . # . # .
+                . # # # .
+                . # . . .
+                . # . . .
+        `)
+        serial.writeLine("Paused")
+    }
+    
+    if (currentState == DONE) {
+        basic.showLeds(`
+            # # # . .
+                # . . # .
+                # . . # .
+                # . . # .
+                # # # . .
+        `)
+        playSound()
+    }
+    
+}
+
+function get_knob_data(): number {
+    let analogValue = pins.analogReadPin(AnalogPin.P1)
+    return Math.map(analogValue, 0, 1023, 0, 270)
+}
+
+function LightWash() {
+    Volume(1000)
+    Spin(400)
+    Softener(5)
+    Detergent(5)
+    serial.writeLine("Water 1000militer")
+    serial.writeLine("Detergent 5militer")
+    serial.writeLine("Softener 5militer")
+    serial.writeLine("400RPM")
+}
+
+function HeavyWash() {
+    let statePrinted: boolean;
+    Volume(2000)
+    Spin(400)
+    Softener(10)
+    Detergent(10)
+    if (statePrinted == false) {
+        serial.writeLine("Water 2liters")
+        serial.writeLine("Detergent 10militer")
+        serial.writeLine("Softener 10militer")
+        serial.writeLine("400RPM")
+        statePrinted = true
+    }
+    
+}
+
+basic.forever(function on_forever() {
+    
+    updateSystem()
+    currentState = evaluateState(currentState)
+    reactToState(currentState)
 })
